8558b2198053ea55dbd04f27727229fdee1958f7
Simón Muñoz <simonvlc@gmail.com>
2015-02-22 01:35:59 +0100

    adding second kata

---
+++ roman_number_converter/src/RomanNumberConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+class RomanNumberConverter
+{
+
+    protected static $roman_keys = array(
+        1000 => "M",
+        900 => "CM",
+        500 => "D",
+        400 => "CD",
+        100 => "C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+    );
+
+    function converts($number)
+    {
+
+        $result = "";
+
+        foreach (self::$roman_keys as $num_value => $glyph) 
+        {
+            while ($number >= $num_value) 
+            {
+                $result .= $glyph;
+                $number -= $num_value;
+            }
+        }
+
+        return $result;
+
+    }
+}
